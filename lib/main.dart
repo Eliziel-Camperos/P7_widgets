46fb4048e27d9bdfdcd6f1dcd3bd995d9b58cc8c
ELIZIEL_CAMPEROS_GARCIA <a.22308051281159@cbtis128.edu.mx>
2025-02-20 19:34:29 +0000

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,41 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(Mywidgets());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class Mywidgets extends StatelessWidget {
+  const Mywidgets({Key? key})
+      : super(key: key); // Added the constructor correctly
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              "camperos",
+              style: TextStyle(fontSize: 40, color: Colors.white),
+            ), // Corrected the appBar's title property
+            backgroundColor: Colors.amber,
+            centerTitle: true,
+          ),
+          body: const Column(
+            children: <Widget>[
+              Text(
+                'Eliziel Camperos Garcia',
+                style: TextStyle(fontSize: 30, color: Colors.deepOrange),
+              ),
+              Text(
+                'Mat: 22308051281159 Gpo 6J',
+                style: TextStyle(fontSize: 20, color: Colors.brown),
+              ),
+              Expanded(
+                child: FittedBox(
+                  child: FlutterLogo(),
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
